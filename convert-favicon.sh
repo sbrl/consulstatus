da84bcf79886a8dd4c68dcc65872c602b3594a21
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2021-02-06 02:41:49 +0000

    Tweak favicon again

--- convert-favicon.sh
+++ convert-favicon.sh
@@ -2,6 +2,10 @@
 
 echo "[ 1 / 2 ] SVG → PNG" >&2;
 inkscape -o src/favicon.png -w 512 src/status.svg;
+optipng -o9 src/favicon.png &
 echo "[ 2 / 2 ] PNG → ICO" >&2;
 convert src/favicon.png -resize 256x256 src/favicon.ico;
+
+wait
+
 echo "done" >&2;
